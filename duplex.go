c8b35ff8044fc3cfb3c7c5b4755cf53b71d75ef6
Equim <sayaka@ekyu.moe>
2018-08-25 22:31:38 +0800

    do not close all sides after pipe

--- duplex.go
+++ duplex.go
@@ -5,33 +5,41 @@ import (
 	"sync"
 )
 
-var (
-	pipeBufPool = sync.Pool{
-		New: func() interface{} {
-			return make([]byte, 128*1024)
-		},
-	}
-)
+type closeWriter interface {
+	CloseWrite() error
+}
 
-func duplexPipe(x, y io.ReadWriteCloser) {
-	wg := sync.WaitGroup{}
+type closeReader interface {
+	CloseRead() error
+}
+
+var pipeBufPool = sync.Pool{
+	New: func() interface{} {
+		return make([]byte, 128*1024)
+	},
+}
+
+func duplexPipe(x, y io.ReadWriter) {
+	var wg sync.WaitGroup
 	wg.Add(2)
 
-	go func() {
-		buf := pipeBufPool.Get().([]byte)
-		io.CopyBuffer(x, y, buf)
-		pipeBufPool.Put(buf)
-		x.Close()
-		wg.Done()
-	}()
-
-	go func() {
-		buf := pipeBufPool.Get().([]byte)
-		io.CopyBuffer(y, x, buf)
-		pipeBufPool.Put(buf)
-		y.Close()
-		wg.Done()
-	}()
+	go pipe(x, y, wg)
+	go pipe(y, x, wg)
 
 	wg.Wait()
+}
+
+func pipe(x, y io.ReadWriter, wg sync.WaitGroup) {
+	buf := pipeBufPool.Get().([]byte)
+	io.CopyBuffer(x, y, buf)
+	pipeBufPool.Put(buf)
+
+	if cw, ok := x.(closeWriter); ok {
+		cw.CloseWrite()
+	}
+	if cr, ok := y.(closeReader); ok {
+		cr.CloseRead()
+	}
+
+	wg.Done()
 }
