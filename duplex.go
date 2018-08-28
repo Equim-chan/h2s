ed8a40f25f18169177852b01a3f4e6fcd4317cd4
Equim <sayaka@ekyu.moe>
2018-08-28 12:49:30 +0800

    fix sync.WaitGroup copy

--- duplex.go
+++ duplex.go
@@ -23,13 +23,13 @@ func duplexPipe(x, y io.ReadWriter) {
 	var wg sync.WaitGroup
 	wg.Add(2)
 
-	go pipe(x, y, wg)
-	go pipe(y, x, wg)
+	go pipe(x, y, &wg)
+	go pipe(y, x, &wg)
 
 	wg.Wait()
 }
 
-func pipe(x, y io.ReadWriter, wg sync.WaitGroup) {
+func pipe(x, y io.ReadWriter, wg *sync.WaitGroup) {
 	buf := pipeBufPool.Get().([]byte)
 	io.CopyBuffer(x, y, buf)
 	pipeBufPool.Put(buf)
